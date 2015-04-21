350fd0a3c31cfce1580cbfc23f6ad22f949ccf67
Darío Javier Cravero <dario@uxtemple.com>
2015-04-21 23:05:29 +0100

    First iteration towards no jQuery and resource modeling.
    Can you separate the character's API and its use?
    
    Why would this make sense?
    
    Can you make it without jQuery?

--- js/app.js
+++ js/app.js
@@ -1,12 +1,72 @@
+'use strict';
+
+// Our resource...
+function Ryu(ryuSelector) {
+  this.$hadouken = document.querySelector('.hadouken');
+  this.$ryu = document.querySelector(ryuSelector);
+  this.$ryuCool = this.$ryu.querySelector('.ryu-cool');
+  this.$ryuReady = this.$ryu.querySelector('.ryu-ready');
+  this.$ryuStill = this.$ryu.querySelector('.ryu-still');
+  this.$ryuThrowing = this.$ryu.querySelector('.ryu-throwing');
+
+  this.state = 'still';
+}
+
+// Its API
+Ryu.prototype.isReady = function() {
+  return this.state === 'ready';
+}
+Ryu.prototype.getReady = function() {
+  if (!this.isReady()) {
+    this.state = 'ready';
+    hide(this.$ryuCool, this.$ryuStill, this.$ryuThrowing);
+    show(this.$ryuReady);
+  }
+}
+
+Ryu.prototype.isStill = function() {
+  return this.state === 'still';
+}
+Ryu.prototype.standStill = function() {
+  if (!this.isStill()) {
+    this.state = 'still';
+    hide(this.$ryuCool, this.$ryuReady, this.$ryuThrowing);
+    show(this.$ryuStill);
+  }
+}
+
+Ryu.prototype.getGansta = function() {
+  // TODO ...
+}
+
+Ryu.prototype.throwHadouken = function() {
+  // TODO ...
+}
+
+// Our app
+var ryu = new Ryu('.ryu');
+
+ryu.$ryu.addEventListener('mouseenter', function() {
+  ryu.getReady();
+});
+ryu.$ryu.addEventListener('mouseleave', function() {
+  ryu.standStill();
+});
+
+// Some helpers...
+function hide() {
+  Array.prototype.forEach.call(arguments, function(el) {
+    el.style.display = 'none';
+  });
+}
+function show() {
+  Array.prototype.forEach.call(arguments, function(el) {
+    el.style.display = 'block';
+  });
+}
+
 $(document).ready(function() {
-  $('.ryu').mouseenter(function(){
-    $('.ryu-still, .ryu-cool, .ryu-throwing').hide();
-    $('.ryu-ready').show();
-  })
-  .mouseleave(function(){
-    $('.ryu-ready, .ryu-cool, .ryu-throwing').hide();
-    $('.ryu-still').show();
-  })
+  $('.ryu')
   .mousedown(function() {
     playHadouken();
     $('.ryu-ready, .ryu-cool, .ryu-still').hide();
