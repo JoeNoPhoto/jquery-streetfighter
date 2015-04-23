9d064daf6a9251073f6797952f92dbb0765519b3
Joe Norris <yourboybstar@gmail.com>
2015-04-23 09:52:06 -0700

    Changed 'Ryu' to Fighter as example

--- js/app.js
+++ js/app.js
@@ -1,50 +1,60 @@
 'use strict';
 
 // Our resource...
-function Ryu(ryuSelector) {
+function Fighter(ryuSelector) {
   this.$hadouken = document.querySelector('.hadouken');
   this.$ryu = document.querySelector(ryuSelector);
   this.$ryuCool = this.$ryu.querySelector('.ryu-cool');
   this.$ryuReady = this.$ryu.querySelector('.ryu-ready');
   this.$ryuStill = this.$ryu.querySelector('.ryu-still');
   this.$ryuThrowing = this.$ryu.querySelector('.ryu-throwing');
-
   this.state = 'still';
 }
 
 // Its API
-Ryu.prototype.isReady = function() {
+Fighter.prototype.isReady = function() {
   return this.state === 'ready';
-}
-Ryu.prototype.getReady = function() {
+};
+Fighter.prototype.getReady = function() {
   if (!this.isReady()) {
     this.state = 'ready';
     hide(this.$ryuCool, this.$ryuStill, this.$ryuThrowing);
     show(this.$ryuReady);
   }
-}
+};
 
-Ryu.prototype.isStill = function() {
+Fighter.prototype.isStill = function() {
   return this.state === 'still';
-}
-Ryu.prototype.standStill = function() {
+};
+Fighter.prototype.standStill = function() {
   if (!this.isStill()) {
     this.state = 'still';
     hide(this.$ryuCool, this.$ryuReady, this.$ryuThrowing);
     show(this.$ryuStill);
   }
-}
+};
 
-Ryu.prototype.getGansta = function() {
-  // TODO ...
-}
+Fighter.prototype.getGansta = function() {
 
-Ryu.prototype.throwHadouken = function() {
-  // TODO ...
-}
+    hide(this.$ryuStill, this.$ryuReady, this.ryuThrowing);
+    show(this.$ryuCool);
+};
+
+
+
+Fighter.prototype.throwHadouken = function() {
+    // TODO ...
+    // change to throwing image
+    hide(this.$ryuStill, this.$ryuReady, this.$ryuCool);
+    show(this.ryuThrowing);
+    //animate hadouken and play Hadouken sound
+
+    //return to standstill
+
+};
 
 // Our app
-var ryu = new Ryu('.ryu');
+var ryu = new Fighter('.ryu');
 
 ryu.$ryu.addEventListener('mouseenter', function() {
   ryu.getReady();
@@ -52,6 +62,17 @@ ryu.$ryu.addEventListener('mouseenter', function() {
 ryu.$ryu.addEventListener('mouseleave', function() {
   ryu.standStill();
 });
+
+ryu.$ryu.addEventListener('', function(){
+
+});
+ryu.$ryu.addEventListener('', function(){
+
+});
+ryu.$ryu.addEventListener('', function(){
+
+});
+
 
 // Some helpers...
 function hide() {
@@ -65,38 +86,38 @@ function show() {
   });
 }
 
-$(document).ready(function() {
-  $('.ryu')
-  .mousedown(function() {
-    playHadouken();
-    $('.ryu-ready, .ryu-cool, .ryu-still').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').finish().show()
-    .animate(
-      {'left': '1020px'},
-      500,
-      function() {
-        $(this).hide();
-        $(this).css('left', '450px');
-      }
-    );
-  })
-  .mouseup(function() {
-    $('.ryu-throwing, .ryu-cool, .ryu-still').hide();
-    $('.ryu-ready').show();
-  });
+// $(document).ready(function() {
+//   $('.ryu')
+//   .mousedown(function() {
+//     playHadouken();
+//     $('.ryu-ready, .ryu-cool, .ryu-still').hide();
+//     $('.ryu-throwing').show();
+//     $('.hadouken').finish().show()
+//     .animate(
+//       {'left': '1020px'},
+//       500,
+//       function() {
+//         $(this).hide();
+//         $(this).css('left', '450px');
+//       }
+//     );
+//   })
+//   .mouseup(function() {
+//     $('.ryu-throwing, .ryu-cool, .ryu-still').hide();
+//     $('.ryu-ready').show();
+//   });
 
-  $(document).keydown(function(event){
-    if(event.keyCode == 88){
-      $('.ryu-still, .ryu-ready, .ryu-throwing').hide();
-      $('.ryu-cool').show();
-    }
-  }).keyup(function(event){
-      if (event.keyCode == 88);
-        $('.ryu-cool, .ryu-ready, .ryu-throwing').hide();
-        $('.ryu-still').show();
-    });
-});
+//   $(document).keydown(function(event){
+//     if(event.keyCode == 88){
+//       $('.ryu-still, .ryu-ready, .ryu-throwing').hide();
+//       $('.ryu-cool').show();
+//     }
+//   }).keyup(function(event){
+//       if (event.keyCode == 88);
+//         $('.ryu-cool, .ryu-ready, .ryu-throwing').hide();
+//         $('.ryu-still').show();
+//     });
+// });
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
